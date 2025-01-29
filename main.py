75ffa19848b575b47757370f8f22dbe8d9986a9f
Rafael Marin Machado de Souza <marinrafael@gmail.com>
2025-01-29 12:34:59 -0300

    feat: User Configured params

--- main.py
+++ main.py
@@ -12,8 +12,18 @@ def main():
     print(f"Using device: {device}")
     
     # Generate dataset
+    n_samples = input("Number of Samples: ")
+    n_features = input("Number of Features (Even number - Except Image): ")
+    n_classes = input("Number of Classes: ")
+
+    num_pop = input("Number of Population (GA): ")
+    num_gen = input("Number of Generations (GA): ")
+    
+    num_part= input("Number of Particle (PSO): ")
+    num_iter= input("Number of Iteractions (PSO): ")
+
     print('Generating multimodal dataset')
-    X_num, X_img, y = generate_multimodal_dataset(num_samples=200, num_features=4, image_size=(64,64), num_classes=2)
+    X_num, X_img, y = generate_multimodal_dataset(num_samples=n_samples, num_features=n_features, image_size=(64,64), num_classes=n_classes)
     X_num, X_img, y = X_num.to(device), X_img.to(device), y.to(device)
 
     now = datetime.datetime.now()
@@ -45,31 +55,39 @@ def main():
     # pca_time = time.time() - start_time
     # pca_fitness = fitness_fn(X_pca, X_img, y)
    
-
     # GA Feature Selection
     print("\n Evaluating GA-selected features...")
     start_time = time.time()
-    ga_selected_features = genetic_algorithm(X_num, X_img, y, fitness_fn, device=device, num_generations=3, population_size=10)
+    ga_selected_features = genetic_algorithm(X_num, X_img, y, fitness_fn, device=device, num_generations=num_gen, population_size=num_pop)
     ga_time = time.time() - start_time
     ga_fitness = fitness_fn(X_num[:, ga_selected_features[:-1].astype(bool)], X_img, y, ga_selected_features[-1].astype(bool))
 
-    # PSO Feature Selectionteste
+    # PSO Feature Selection
     print("\n Evaluating PSO-selected features...")
     start_time = time.time()
-    pso_selected_features = particle_swarm_optimization(X_num, X_img, y, fitness_fn, device=device, num_iterations=3, num_particles=10)
+    pso_selected_features = particle_swarm_optimization(X_num, X_img, y, fitness_fn, device=device, num_iterations=num_iter, num_particles=num_part)
     pso_time = time.time() - start_time
     pso_fitness = fitness_fn(X_num[:, pso_selected_features[:-1].astype(bool)], X_img, y, pso_selected_features[-1].astype(bool))
 
     # Summary
     print("\nPerformance Summary:")
+    print("---------------------------------------------------------------------------")
+    print(f"Number of Samples: {n_samples}")
+    print(f"Number of Features (Even number - Except Image): {n_features}")
+    print(f"Number of Classes: {n_classes}")
+    print("---------------------------------------------------------------------------")  
     print(f"All Features Fitness Score: {baseline_fitness:.4f} | Runtime: {baseline_time:.2f} seconds")
     print("---------------------------------------------------------------------------")
     # print(f"PCA Fitness Score: {pca_fitness:.4f} | Runtime: {pca_time:.2f} seconds")
     # print(f"PCA-selected features (binary mask): {pca_selected_features}")
     # print("---------------------------------------------------------------------------")
+    print(f"Number of Population (GA): {num_pop}")
+    print(f"Number of Generations (GA): {num_gen}")
     print(f"GA Fitness Score: {ga_fitness:.4f} | Runtime: {ga_time:.2f} seconds")
     print(f"GA-selected features (binary mask): {ga_selected_features}")
     print("---------------------------------------------------------------------------")
+    print(f"Number of Particle (PSO): {num_part}")
+    print(f"Number of Iteractions (PSO): {num_iter}")
     print(f"PSO Fitness Score: {pso_fitness:.4f} | Runtime: {pso_time:.2f} seconds")
     print(f"PSO-selected features (binary mask): {pso_selected_features}")
 
